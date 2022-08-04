6f99c63ddab8c93f1556c4ddcd9fc36ba7c64847
santosh <santoshbudha809@gmail.com>
2022-08-04 08:04:52 +0545

    inital commit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,20 @@
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length= 300)
+    slug = models.CharField(max_length= 300)
+    icon = models.CharField(max_length= 100)
+
+    def __str__(self):
+        return self.name
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length= 300)
+    category = models.ForeignKey(Category,on_delete = models.CASCADE)
+    slug = models.CharField(max_length= 300)
+    icon = models.CharField(max_length= 100)
+
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic import View
 # Create your views here.
+class Base(View):
+    context = {}
+
+class HomeView(Base):
+    def get (self,request):
+
+        return render(request,'index.html')
