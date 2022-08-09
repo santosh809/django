b046776187cba0476934965153de3feb807558c0
santosh <santoshbudha809@gmail.com>
2022-08-09 07:52:48 +0545

    inital commit

--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from distutils.command.upload import upload
+from email.mime import image
 from enum import unique
 from unicodedata import category
 from django.db import models
@@ -33,9 +34,8 @@ class Slider(models.Model):
 class Ads(models.Model):
     name = models.CharField(max_length= 300)
     image = models.ImageField(upload_to = 'media')
-    rank = models.ImageField(unique = True)
+    rank = models.ImageField()
     description = models.TextField(blank = True)
-
 
     def __str__(self):
         return self.name
@@ -47,6 +47,8 @@ class Brand(models.Model):
 
     def __str__(self):
         return self.name
+
+        
 STOCK =(( 'In','In Stock'),('Out','Out stock'))
 LABELS = (('hot','hot'),('sale','sale'),('new','new'),('','defult'))
 class Product(models.Model):
@@ -62,6 +64,29 @@ class Product(models.Model):
     description = models.TextField(blank = True)
     slug = models.CharField(max_length=500)
 
+class Review(models.Model):
+    name = models.CharField(max_length=40)
+    image = models.ImageField(upload_to = 'media')
+    icon = models.CharField(max_length=40,blank = True)
+    remark = models.CharField(max_length=500)
+    profesion = models.CharField(max_length=50)
+
     def __str__(self):
         return self.name
 
+class Service(models.Model):
+    icon = models.CharField(max_length=40)
+    name = models.CharField(max_length=40)
+    desc = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+# class Lowerads(models.Model):
+#     image = models.ImageField(upload_to = 'media')
+#     remark =models.CharField(max_length=100)
+#     name = models.CharField(max_length=40)
+#     rank = models.ImageField(unique = True)
+    
+#     def __str__ (self):
+#         return self.name
--- home/views.py
+++ home/views.py
@@ -1,14 +1,22 @@
+from cProfile import label
 from django.shortcuts import render
 from django.views.generic import View
 from .models import * 
 # Create your views here.
 class Base(View):
-    context = {}
+    context = {} #dictionary
 
 class HomeView(Base):
     def get (self,request):
-        self.context['catas'] = Category.object.all()
-        self.context['Subcatas'] = SubCategory.object.all()
-        #self.context['ads'] = Category.object.all()
-        #self.context['sliders'] = Category.object.all()
-        return render(request,'index.html')
+        self.context['catas'] = Category.objects.all()
+        self.context['subcatas'] = SubCategory.objects.all()
+        self.context['ads'] = Ads.objects.all()
+        self.context['sliders'] = Slider.objects.all()
+        self.context['brand'] = Brand.objects.all()
+        self.context['reviews'] = Review.objects.all()
+        self.context['serv']= Service.objects.all()
+        self.context['hots'] = Product.objects.filter(lables = 'hot')
+        self.context['news'] = Product.objects.filter(lables = 'new')
+        self.context['sales'] = Product.objects.filter(lables = 'sale')
+        return render(request,'index.html',self.context)
+
